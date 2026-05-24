be264c1864086a605e86742efbdb92ab5bebd94a
Tobi Olu AnuAkin Akinyemi <olutakinyemi@gmail.com>
2026-05-24 00:34:00 -0500

    admin bug fixed

--- functions/admin.js
+++ functions/admin.js
@@ -1,4 +1,7 @@
 export async function onRequestGet({ request, env }) {
-  const url = new URL('/admin.html', request.url);
-  return env.ASSETS.fetch(new Request(url, request));
+  const url = new URL('/admin-page', request.url);
+  const resp = await env.ASSETS.fetch(new Request(url, request));
+  const headers = new Headers(resp.headers);
+  headers.set('Content-Type', 'text/html; charset=utf-8');
+  return new Response(resp.body, { status: resp.status, headers });
 }
